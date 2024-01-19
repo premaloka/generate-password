446a57b13ce1b2c07df38f47275ddd438c6c0008
premaloka <premaloka.development@gmail.com>
2024-01-19 19:01:12 +0100

    Add copy icon when clicked and info message

--- index.js
+++ index.js
@@ -11,15 +11,31 @@ let output2El = document.getElementById("field-2")
 const symbolEl = document.getElementById('toggle-symbols');
 const numbersEl = document.getElementById('toggle-numbers');
 const passwordEl = document.getElementById("psswrd")
+const clipboardEl = document.getElementById("copiedToClipboard")
 const checkboxes = document.querySelectorAll("input[type='checkbox']")
+const outputFields = document.querySelectorAll(".output--field")
 
-console.log(passwordEl.value)
 let numbersIncluded = false;
 let symbolsIncluded = false;
 let passwordLength = passwordEl.value;
 
-//symbolEl.addEventListener('change', selectedOptions);
-//numbersEl.addEventListener('change', selectedOptions);
+// create an img element for the SVG
+let copyIcon = document.createElement("img")
+copyIcon.setAttribute("src", "data:image/svg+xml," + encodeURIComponent(`
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="#d5d4d8" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+    </svg>
+`));
+copyIcon.className = "copy-icon"
+let copyIcon2 = document.createElement("img")
+copyIcon2.setAttribute("src", "data:image/svg+xml," + encodeURIComponent(`
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="#d5d4d8" class="w-6 h-6">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+    </svg>
+`));
+copyIcon2.className = "copy-icon"
+
+
 
 // Add event listeners to checkboxes
 for (const checkbox of checkboxes) {
@@ -28,6 +44,20 @@ for (const checkbox of checkboxes) {
 
 // Add event listener for password input text
 passwordEl.addEventListener("change", selectedOptions)
+
+// Add event listener for clicking the output field
+for (const output of outputFields) {
+    output.addEventListener("click", copyMessage)
+}
+
+function copyMessage() {
+    clipboardEl.textContent = "(Password copied to clipboard!)"
+    clipboardEl.style.display = "block"
+    setTimeout(() => {
+        clipboardEl.style.display = "none"
+        // clipboardEl.textContent = ""
+    }, 3000)
+}
 
 function selectedOptions(e) {
     e.preventDefault();
@@ -43,8 +73,8 @@ function selectedOptions(e) {
 function generate() {
     let keys1 = [];
     let keys2 = [];
-    if (passwordLength > 20) passwordLength = 20;
-    
+    if (parseInt(passwordLength) > 20) passwordLength = 20;
+
     for (let i = 0; i < parseInt(passwordLength); i++) {
         /*
          keys1.push(characters[getRandom(0, characters.length)])
@@ -55,6 +85,8 @@ function generate() {
     }
     output1El.textContent = keys1.join("")
     output2El.textContent = keys2.join("")
+    output2El.appendChild(copyIcon);
+    output1El.appendChild(copyIcon2);
 }
 
 function getCharacters(symbols, numbers) {
