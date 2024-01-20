5debdb50df177e2b182f23ac49a820140f37b1e0
premaloka <premaloka.development@gmail.com>
2024-01-20 11:08:25 +0100

    Update svg img

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "~", "`", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "_", "-", "+", "=", "{", "[", "}", "]", ",", "|", ":", ";", "<", ">", ".", "?",
     "/"];
 
+const svgImg = "data:image/svg+xml," + encodeURIComponent(`
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#4adf86" class="w-6 h-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
+</svg>
+`);
+
 function getRandom(min, max, floatFlag) {
     r = Math.random() * (max - min) + min
     return floatFlag ? r : Math.floor(r)
@@ -12,7 +18,7 @@ const symbolEl = document.getElementById('toggle-symbols');
 const numbersEl = document.getElementById('toggle-numbers');
 const passwordEl = document.getElementById("psswrd")
 const clipboardEl = document.getElementById("copiedToClipboard")
-const checkboxes = document.querySelectorAll("input[type='checkbox']")
+const boxes = document.querySelectorAll("input[type='checkbox'], #psswrd")
 const outputFields = document.querySelectorAll(".output--field")
 
 let numbersIncluded = false;
@@ -21,42 +27,21 @@ let passwordLength = passwordEl.value;
 
 // create an img element for the SVG
 let copyIcon = document.createElement("img")
-copyIcon.setAttribute("src", "data:image/svg+xml," + encodeURIComponent(`
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="#d5d4d8" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
-    </svg>
-`));
 copyIcon.className = "copy-icon"
+copyIcon.setAttribute("src", svgImg);
 let copyIcon2 = document.createElement("img")
-copyIcon2.setAttribute("src", "data:image/svg+xml," + encodeURIComponent(`
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1" stroke="#d5d4d8" class="w-6 h-6">
-        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
-    </svg>
-`));
 copyIcon2.className = "copy-icon"
+copyIcon2.setAttribute("src", svgImg);
 
 
-
-// Add event listeners to checkboxes
-for (const checkbox of checkboxes) {
-    checkbox.addEventListener("change", selectedOptions)
+// Add event listeners to checkboxes and password length
+for (const box of boxes) {
+    box.addEventListener("change", selectedOptions)
 }
-
-// Add event listener for password input text
-passwordEl.addEventListener("change", selectedOptions)
 
 // Add event listener for clicking the output field
 for (const output of outputFields) {
     output.addEventListener("click", copyMessage)
-}
-
-function copyMessage() {
-    clipboardEl.textContent = "(Password copied to clipboard!)"
-    clipboardEl.style.display = "block"
-    setTimeout(() => {
-        clipboardEl.style.display = "none"
-        // clipboardEl.textContent = ""
-    }, 3000)
 }
 
 function selectedOptions(e) {
@@ -69,6 +54,14 @@ function selectedOptions(e) {
     console.log("password", passwordLength)
 }
 
+function copyMessage() {
+    clipboardEl.textContent = "(Password copied to clipboard!)"
+    clipboardEl.style.display = "block"
+    setTimeout(() => {
+        clipboardEl.style.display = "none"
+        // clipboardEl.textContent = ""
+    }, 3000)    
+}    
 
 function generate() {
     let keys1 = [];
