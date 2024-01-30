bc79f962d2af585643169ec83a7678ab46cfc30c
premaloka <premaloka.development@gmail.com>
2024-01-30 11:40:50 +0100

    Update layout, colors and display

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const characters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", 
     "/"];
 
 const svgImg = "data:image/svg+xml," + encodeURIComponent(`
-<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#4adf86" class="w-6 h-6">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="#FFCA35" class="w-6 h-6">
     <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 17.25v3.375c0 .621-.504 1.125-1.125 1.125h-9.75a1.125 1.125 0 0 1-1.125-1.125V7.875c0-.621.504-1.125 1.125-1.125H6.75a9.06 9.06 0 0 1 1.5.124m7.5 10.376h3.375c.621 0 1.125-.504 1.125-1.125V11.25c0-4.46-3.243-8.161-7.5-8.876a9.06 9.06 0 0 0-1.5-.124H9.375c-.621 0-1.125.504-1.125 1.125v3.5m7.5 10.375H9.375a1.125 1.125 0 0 1-1.125-1.125v-9.25m12 6.625v-1.875a3.375 3.375 0 0 0-3.375-3.375h-1.5a1.125 1.125 0 0 1-1.125-1.125v-1.5a3.375 3.375 0 0 0-3.375-3.375H9.75" />
 </svg>
 `);
@@ -14,9 +14,10 @@ function getRandom(min, max, floatFlag) {
 
 let output1El = document.getElementById("field-1")
 let output2El = document.getElementById("field-2")
-const symbolEl = document.getElementById('toggle-symbols');
-const numbersEl = document.getElementById('toggle-numbers');
+const symbolEl = document.getElementById('toggle-symbols')
+const numbersEl = document.getElementById('toggle-numbers')
 const passwordEl = document.getElementById("psswrd")
+const passwordNumberEl = document.querySelector("#psswrd--length")
 const clipboardEl = document.getElementById("clipboard-message")
 const boxes = document.querySelectorAll("input[type='checkbox'], #psswrd")
 const outputFields = document.querySelectorAll(".output--field")
@@ -49,6 +50,7 @@ function selectedOptions(e) {
     symbolsIncluded = symbolEl.checked;
     numbersIncluded = numbersEl.checked;
     passwordLength = passwordEl.value;
+		passwordNumberEl.textContent = passwordLength;
     console.log("symbols", symbolsIncluded)
     console.log("numbers", numbersIncluded)
     console.log("password", passwordLength)
@@ -66,7 +68,7 @@ function copyMessage() {
 function generate() {
     let keys1 = [];
     let keys2 = [];
-    if (parseInt(passwordLength) > 20) passwordLength = 20;
+    if (parseInt(passwordLength) > 24) passwordLength = 24;
 
     for (let i = 0; i < parseInt(passwordLength); i++) {
         /*
